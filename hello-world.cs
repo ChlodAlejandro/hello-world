9c0bd07cb501b2f33d29dbf4951a4fd214cb83da
ChlodAlejandro <chlod@chlod.net>
2022-01-08 12:48:48 +0800

    initial commit

---
+++ hello-world.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+
+    static class HelloWorld {
+
+        static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+    }
+
+}
